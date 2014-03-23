c903e0caf0faa3c7e29748a38d724d3727467d63
Vladimir <vladimir@vladimir-VirtualBox.(none)>
2014-03-23 21:16:56 +0400

    modified:   2048_files/application.js 	modified:   2048_files/game_manager.js 	modified:   2048_files/grid.js 	modified:   2048_files/keyboard_input_manager.js 	modified:   2048_files/local_score_manager.js 	modified:   2048_files/main.css 	modified:   2048_files/tile.js 	new file:   "2048_files/\320\272\320\276\320\277\320\270\321\217 main.css" 	modified:   index.html

--- 2048_files/local_score_manager.js
+++ 2048_files/local_score_manager.js
@@ -18,14 +18,15 @@ window.fakeStorage = {
   }
 };
 
-function LocalScoreManager() {
-  this.key     = "bestScore";
+function LocalStorageManager() {
+  this.bestScoreKey     = "bestScore";
+  this.gameStateKey     = "gameState";
 
   var supported = this.localStorageSupported();
   this.storage = supported ? window.localStorage : window.fakeStorage;
 }
 
-LocalScoreManager.prototype.localStorageSupported = function () {
+LocalStorageManager.prototype.localStorageSupported = function () {
   var testKey = "test";
   var storage = window.localStorage;
 
@@ -38,11 +39,25 @@ LocalScoreManager.prototype.localStorageSupported = function () {
   }
 };
 
-LocalScoreManager.prototype.get = function () {
-  return this.storage.getItem(this.key) || 0;
+// Best score getters/setters
+LocalStorageManager.prototype.getBestScore = function () {
+  return this.storage.getItem(this.bestScoreKey) || 0;
 };
 
-LocalScoreManager.prototype.set = function (score) {
-  this.storage.setItem(this.key, score);
+LocalStorageManager.prototype.setBestScore = function (score) {
+  this.storage.setItem(this.bestScoreKey, score);
 };
 
+// Game state getters/setters and clearing
+LocalStorageManager.prototype.getGameState = function () {
+  var stateJSON = this.storage.getItem(this.gameStateKey);
+  return stateJSON ? JSON.parse(stateJSON) : null;
+};
+
+LocalStorageManager.prototype.setGameState = function (gameState) {
+  this.storage.setItem(this.gameStateKey, JSON.stringify(gameState));
+};
+
+LocalStorageManager.prototype.clearGameState = function () {
+  this.storage.removeItem(this.gameStateKey);
+};
